11f27614fac7a797a05f09cb8ddc7c47800d6d36
Oziel Silveira Junior <37221417+ozielsilveira@users.noreply.github.com>
2023-06-26 17:15:20 -0300

    add cors configs

--- server/src/config/server.ts
+++ server/src/config/server.ts
@@ -1,7 +1,21 @@
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 import 'express-async-errors';
-import router from '../api/routes';
+
+import { corsOptions } from './corsOptions';
+import { errorHandler } from '../api/utils/error';
+import  routes  from '../api/routes';
+import { initCron } from './cron';
 
 export const server = express();
 
-server.use('/api', router);
+initCron();
+
+server.use(cors(corsOptions));
+server.use(express.json());
+
+server.use('/api', routes);
+
+server.use(helmet());
+server.use(errorHandler);
