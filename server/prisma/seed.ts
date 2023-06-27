f19a6e6c07dc6b56101f487fb16040752ce0750b
Oziel Silveira Junior <37221417+ozielsilveira@users.noreply.github.com>
2023-06-26 22:53:08 -0300

    fix: add plan.name and adjust strings

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,17 +1,17 @@
-import { prisma } from '.';
-import { createAdminService, createManyPermissionsService } from '../src/api/repositories/seeds';
+// import { prisma } from '.';
+// import { createAdminService, createManyPermissionsService } from '../src/api/repositories/seeds';
 
-const main = async () => {
-  await createManyPermissionsService();
-  await createAdminService();
-};
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (error) => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// const main = async () => {
+//   await createManyPermissionsService();
+//   await createAdminService();
+// };
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (error) => {
+//     // eslint-disable-next-line no-console
+//     console.error(error);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
